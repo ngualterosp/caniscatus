a28ced2f5ed1d2b28092d8fc5fdaf4b8247afc79
Nicolas Gualteros <ngualterosp@unbosuqe.edu.co>
2019-06-20 15:17:20 -0500

    Coreccion quienes somos

--- resources/views/quienessomos.blade.php
+++ resources/views/quienessomos.blade.php
@@ -2,96 +2,92 @@
  @section('content')
  
  <section class="about-area section-gap">
-		<div class="container">
+
+ <div class="container">
 			<div class="row align-items-center">
 				<div class="col">
 
-	
+				<div>
 
-<div class="row">
-  <div class="col-md-4">
-  <div class="about">
-   <a><a href="URL DESTINO"><img src="img/1op.png" width="80px" /></a>
-  </div>
-</div>
-
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/2op.png" width="80px" align="center">
-  </div>
-</div>
-
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/3op.png" width="80px" align="center">
-  </div>
-</div>
-</div>
-
-<div class="row">
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/4op.png" width="80px" align="center">
-  </div>
-</div>
-
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/5op.png" width="80px" align="center">
-  </div>
-</div>
-
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/6op.png" width="80px" align="center">
-  </div>
-</div>
-</div>
-
-<div class="row">
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/7op.png" width="80px" align="center">
-  </div>
-</div>
-
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/8op.png" width="80px" align="center">
-  </div>
-</div>
-
-<div class="col-md-4">
-  <div class="about">
-  <img src="img/9op.png" width="80px" align="center">
-  </div>
-</div>
-</div>
-
-          
-				</div>
-				<div class="col-lg-5 col-md-12 about-right">
-					<div class="section-title">
+        <div class="section-title" >
 						<h1>¿Quiénes Somos?</h1>
-					</div>
+          </div>
           <hr class="divider my-4">
 					<div class="mb-50 wow fadeIn" data-wow-duration=".8s">
-						<p align="justify">
+						<h4 align="justify">
             CANISCATUS S.A.S. Es una empresa creada con el objetivo principal de brindar un servicio de calidad y calidez para su mascota, teniendo en cuenta las características y necesidades de la misma.
-            Así mismo, las instalaciones dispuestas para ello, están adaptadas y acondicionadas pensando en el bienestar animal.
-						</p>
-						<p align="justify">
-            De igual forma, CANISCATUS S.A.S cuenta con talento humano profesional y técnico para el desarrollo de las actividades, durante la estadía de su mascota.
-						</p>
-            <p align="justify"> 
-            Esta empresa ofrece los siguientes servicios: 
-            </p>
+            Así mismo, las instalaciones dispuestas para ello, están adaptadas y acondicionadas pensando en el bienestar animal.             De igual forma, CANISCATUS S.A.S cuenta con talento humano profesional y técnico para el desarrollo de las actividades, durante la estadía de su mascota.
+            Contamos con el personal profesional calificado y capacitado para hacer una
+experiencia agradable a su mascota; haciéndolo sentir como en casa.
+</h4>
+						
 					</div>
 					
 				</div>
 			</div>
 		</div>
+
+    <br>
+    <br>
+
+		<div class="container" id="estilo1">
+    <div class="row">
+          <div class="col-lg-5 mr-auto mb-5 align-self-center" id="estilo1">
+
+          <div class="section-title" >
+						<h1 id="estilo1">MISIÓN</h1>
+          </div>
+          <hr align="left" noshade="noshade" size="10" width="80%" />
+					<div class="mb-50 wow fadeIn" data-wow-duration=".8s">
+						<h5 align="justify">
+            Es una empresa que provee servicios de Manejo y Cuidado animal, con la más alta calidad y calidez. Así mismo, incentivamos la corresponsabilidad
+            y sensibilidad frente a la vida y cuidado de los animales de compañía dentro y fuera de casa.
+</h5>
+					</div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="image-absolute-box">
+             
+              <img src="img/bg-masthead.jpg" alt="Image" class="img-fluid img-shadow">
+            </div>
+          </div>
+        </div>
+		</div>
+<br>
+<br>
+
+<div class="container" id="estilo2">
+    <div class="row">
+    <div class="col-lg-6">
+            <div class="image-absolute-box">
+             
+              <img src="img/bg-masthead.jpg" alt="Image" class="img-fluid img-shadow">
+            </div>
+          </div>
+          <div class="col-lg-5 mr-auto mb-5 align-self-center" >
+
+          <div class="section-title" >
+						<h1 id="estilo2">VISIÓN</h1>
+          </div>
+          <hr align="left" noshade="noshade" size="10" width="80%" />
+
+					<div class="mb-50 wow fadeIn" data-wow-duration=".8s" >
+						<h5 align="justify" id="estilo2">
+            Será la mejor empresa proveedora de servicios de manejo y cuidado de los animales de compañía, cumpliendo con los Estándares de Calidad y Bienestar Animal a nivel
+            Distrital y Nacional. Siendo líderes en el gremio de las Guarderías Caninas y Hoteles Caninos y Felinos.
+</h5>
+						
+					</div>
+          </div>
+
+          
+        </div>
+		</div>
+
+    
 	</section>
+  
 
 
 @endsection
