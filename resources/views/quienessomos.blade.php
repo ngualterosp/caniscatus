76a2fd4667ea187186308342d6e81981a5ae97c8
Nicolas Gualteros <ngualterosp@unbosuqe.edu.co>
2019-06-20 21:45:58 -0500

    t

--- resources/views/quienessomos.blade.php
+++ resources/views/quienessomos.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.app') 
  @section('content')
+
+ <!-- Masthead -->
+ <div id="demo" class="carousel slide" data-ride="carousel">
+
+  
+  
+<!-- The slideshow -->
+<div class="container">
+<div class="row">
+  <div class="col" aling="center">
+    <!-- Indicators -->
+    <center>
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+
+</ul>
+      <div class="carousel-inner">
+          <div class="carousel-item active" >
+            <img src="img/bannerquienes.jpg" alt="1" class="d-block w-100" >
+          </div>
+        </div>
+      
+      
+      <!-- Left and right controls -->
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </center>
+  </div>
+</div>
+
+</div>
  
  <section class="about-area section-gap">
 
