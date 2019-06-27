d6a09808cc928f5316053528b59daf39c5c8976d
Nicolas Gualteros <ngualterosp@unbosuqe.edu.co>
2019-06-26 20:52:55 -0500

    correciones quienes somos

--- resources/views/quienessomos.blade.php
+++ resources/views/quienessomos.blade.php
@@ -56,66 +56,56 @@ experiencia agradable a su mascota; haciéndolo sentir como en casa.
 					
 				</div>
 			</div>
-		</div>
-
-    <br>
+    </div>
     <br>
 
-		<div class="container" id="estilo1">
-    <div class="row">
-          <div class="col-lg-5 mr-auto mb-5 align-self-center" id="estilo1">
+<div class="container" width="500px">
+  <div class="card" id="estilo1">
+    <div class="row ">
 
-          <div class="section-title" >
-						<h1 id="estilo1">MISIÓN</h1>
-          </div>
+      <div class="col-sm-7" id="p">
+        <div class="card-block">
+          <h1> MISIÓN </h1>
           <hr align="left" noshade="noshade" size="10" width="80%" />
-					<div class="mb-50 wow fadeIn" data-wow-duration=".8s">
-						<h4 align="justify">
+          <h4 align="justify">
             Es una empresa que provee servicios de Manejo y Cuidado animal, con la más alta calidad y calidez. Así mismo, incentivamos la corresponsabilidad
             y sensibilidad frente a la vida y cuidado de los animales de compañía dentro y fuera de casa.
-</h4>
-					</div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="image-absolute-box">
-             
-              <img src="img/mision.jpg" alt="Image" class="img-fluid img-shadow">
-            </div>
-          </div>
+          </h4>
         </div>
-		</div>
-<br>
-<br>
+      </div>
 
-<div class="container" id="estilo2">
-    <div class="row">
-    <div class="col-lg-6">
-            <div class="image-absolute-box">
-             
-              <img src="img/vision.jpg" alt="Image" class="img-fluid img-shadow">
-            </div>
-          </div>
-          <div class="col-lg-5 mr-auto mb-5 align-self-center" >
+      <div class="col-sm-5">
+        <img class="d-block w-100" src="img/mision.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</div>
 
-          <div class="section-title" >
-						<h1 id="estilo2">VISIÓN</h1>
-          </div>
+<div class="container" width="500px">
+  <div class="card" id="estilo2">
+    <div class="row ">
+
+      <div class="col-sm-5">
+        <img class="d-block w-100" src="img/vision.jpg" alt="">
+      </div>
+
+      <div class="col-sm-7" id="p">
+        <div class="card-block">
+          <h1 id="estilo2"> VISIÓN </h1>
           <hr align="left" noshade="noshade" size="10" width="80%" />
-
-					<div class="mb-50 wow fadeIn" data-wow-duration=".8s" >
-						<h4 align="justify" id="estilo2">
+          <h4 align="justify" id="estilo2">
             Será la mejor empresa proveedora de servicios de manejo y cuidado de los animales de compañía, cumpliendo con los Estándares de Calidad y Bienestar Animal a nivel
             Distrital y Nacional. Siendo líderes en el gremio de las Guarderías Caninas y Hoteles Caninos y Felinos.
-</h4>
-						
-					</div>
-          </div>
-
-          
+          </h4>
         </div>
-		</div>
+      </div>
 
+      
+    </div>
+  </div>
+</div>
+
+  
     
 	
   
